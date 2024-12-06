c0f87e0c270effe4e3207c5270409eeebcad25ef
Inseok Kang <combikms@naver.com>
2024-12-06 14:22:21 +0900

    :bug: Modify: 프로세스 경로를 상대 경로로 지정

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <spawn.h>
 extern char **environ;
 
-
 int main()
 {
     pid_t pid_r;
@@ -15,18 +14,32 @@ int main()
     pid_t r_pid;
     pid_t c_pid;
 
-    char *r_argv[] = {"sudo","./handler", NULL};
-    char *c_argv[] = {"sudo","./controller", NULL};
+    // 상대 경로로 수정
+    char *r_argv[] = {"sudo", "./handler", NULL};
+    char *c_argv[] = {"sudo", "./controller", NULL};
     
-    posix_spawn(&pid_r, "/home/pi/rc_car_project/handler", NULL, NULL, r_argv, environ);
-    posix_spawn(&pid_c, "/home/pi/rc_car_project/controller", NULL, NULL, c_argv, environ);
-    if((r_pid = waitpid(pid_r, &status_r, 0)) < 0){ // 자식 프로세스의 종료 대기
+    // 상대 경로를 사용하여 posix_spawn 호출
+    if (posix_spawn(&pid_r, "./handler", NULL, NULL, r_argv, environ) != 0) {
+        perror("posix_spawn handler failed");
+        return 1;
+    }
+    
+    if (posix_spawn(&pid_c, "./controller", NULL, NULL, c_argv, environ) != 0) {
+        perror("posix_spawn controller failed");
+        return 1;
+    }
+
+    // 자식 프로세스 종료 대기
+    if((r_pid = waitpid(pid_r, &status_r, 0)) < 0){ 
         printf("handler waitpid() error\n");
     }
-    if((c_pid = waitpid(pid_c, &status_c, 0)) < 0){ // 자식 프로세스의 종료 대기
-    printf("controller waitpid() error\n");
+    if((c_pid = waitpid(pid_c, &status_c, 0)) < 0){ 
+        printf("controller waitpid() error\n");
     }
-    printf("Child pid: %d, status: %d\n", r_pid, status_r);
-    printf("Child pid: %d, status: %d\n", r_pid, status_c);
+
+    // 자식 프로세스 상태 출력
+    printf("Handler Child pid: %d, status: %d\n", r_pid, status_r);
+    printf("Controller Child pid: %d, status: %d\n", c_pid, status_c);
+
     return 0;
 }
