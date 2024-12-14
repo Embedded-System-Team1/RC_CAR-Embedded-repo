4589eccd2070ae172e3fdefb07510d498cffe51c
Inseok Kang <combikms@naver.com>
2024-12-14 14:48:32 +0900

    :sparkles: Feat: Horn 기능 확장

--- device_adaptor.c
+++ device_adaptor.c
@@ -5,6 +5,8 @@
 #include "dc_motor.c" // DC 모터 명령 처리 함수 포함
 #include "horn.c"     // Horn 명령 처리 함수 포함
 
+#include "horn.c"     // Horn 명령 처리 함수 포함
+
 // -- DC 모터 장치 추가 및 기능 구현 커스텀 예시 --
 // 핸들러 등록 테이블
 #define MAX_MOTOR_TYPES 10
@@ -50,15 +52,17 @@ void handle_motor_command(int motor_id, const char* message) {
 
 // 핸들러 등록 함수
 void register_custom_device_handler() {
-    wiringPiSetup();
-
     // DC 모터 핸들러 생성
     MotorHandler dc_motor_handler = {
         .handle_command = handle_dc_motor_command,
         .parse_message = parse_dc_motor_message,
     };
     register_motor_handler(0, dc_motor_handler); // 모터 ID 0에 등록
-    DCMotorSetup();                              // 모터 핀 셋업
 
-
+    // Horn 핸들러 생성
+    MotorHandler horn_handler = {
+        .handle_command = handle_horn_command,
+        .parse_message = parse_horn_message,
+    };
+    register_motor_handler(1, horn_handler); // 모터 ID 1에 등록
 }
